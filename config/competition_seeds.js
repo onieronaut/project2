6acf80b6bb6cd8ce98b84a3b6c2c603cbe02a971
Magen Bouchard <Magen.Bouchard@gmail.com>
2020-01-11 11:17:17 -0500

    added breeder and competition seed files

---
+++ config/competition_seeds.js
@@ -0,0 +1,21 @@
+let competitionSeeds = [
+  {
+    competitionName: "K9 Post",
+    email: "K9Post@gmail.com",
+    dateOfcompetition: "02/15/2020",
+    locationOfcompetition: "24747",
+    description: "Blah Blah",
+    group: "working"
+  },
+  {
+    competitionName: "Jacksonville All-breed Conformation Show",
+    email: "AussieLover234@gmail.com",
+    dateOfcompetition: "03/21/2020",
+    locationOfcompetition: "24747",
+    description: "Blah Blah",
+    group: "All"
+  }
+];
+
+
+module.exports = competitionSeeds;

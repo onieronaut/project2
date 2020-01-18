8e3c7cc5a02cde33a9242a644767a64a86ac6bd2
Magen Bouchard <Magen.Bouchard@gmail.com>
2020-01-18 10:08:25 -0500

    practice

--- api-test/magenpractice/map.js
+++ api-test/magenpractice/map.js
@@ -3,6 +3,8 @@ var platform = new H.service.Platform({
   apikey: "gOOhBUyAfHGRcot-FlkhEvXg9gbjvdcXml3jbUgdkac"
 });
 
+var lng = -81.3792 
+var lat = 28.5383
 // Obtain the default map types from the platform object
 var maptypes = platform.createDefaultLayers();
 
@@ -11,62 +13,21 @@ var map = new H.Map(
   maptypes.vector.normal.map,
   {
     zoom: 10,
-    center: { lng: 20.4, lat: 52.51 }
-  },
+    center: { lng: lng, lat: lat }
+  }
+) 
 
-  
-);
-
-var geocodingParams = {
-    searchText: '200 S Mathilda Ave, Sunnyvale, CA'
-  };
-
-// Define a callback function to process the geocoding response:
-var onResult = function(result) {
-var locations = result.Response.View[0].Result,
-    position,
-    marker;
-// Add a marker for each location found
-for (i = 0;  i < locations.length; i++) {
-  position = {
-    lat: locations[i].Location.DisplayPosition.Latitude,
-    lng: locations[i].Location.DisplayPosition.Longitude
-  };
-  marker = new H.map.Marker(position);
-  map.addObject(marker);
-}
-};
-
-// Get an instance of the geocoding service:
-var geocoder = platform.getGeocodingService();
-
-// Call the geocode method with the geocoding parameters,
-// the callback and an error callback function (called if a
-// communication error occurs):
-geocoder.geocode(geocodingParams, onResult, function(e) {
-alert(e);
-});
+var svgMarkup = '<svg width="24" height="24" ' +
+'xmlns="http://www.w3.org/2000/svg">' +
+'<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+'height="22" /><text x="12" y="18" font-size="12pt" ' +
+'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+'fill="white">H</text></svg>'
 
 
+var icon = new H.map.Icon(svgMarkup),
+coords = {lat:lat, lng:lng},
+marker = new H.map.Marker(coords, {icon :icon});
 
-
-
-// // Enable the event system on the map instance:
-// var mapEvents = new H.mapevents.MapEvents(map);
-
-// // Add event listener:
-// map.addEventListener("tap", function(evt) {
-//   // Log 'tap' and 'mouse' events:
-//   console.log(evt.type, evt.currentPointer.type);
-// });
-
-// var behavior = new H.mapevents.Behavior(mapEvents);
-// var icon = new H.map.Icon('animal-paw-print-pngrepo-com.png');
-// // Create a marker using the previously instantiated icon:
-// var marker = new H.map.Marker({ lng: 13.4, lat: 52.51 }, { icon: icon });
-
-// // Add the marker to the map:
-// map.addObject(marker);
-
-
-
+map.addObject(marker);
+map.setCenter(coords);

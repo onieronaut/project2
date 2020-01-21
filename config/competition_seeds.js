6603b02065d1791a6ee06edcb0421d5d2705f220
jerard <qbin221@gmail.com>
2020-01-21 13:46:33 -0500

    removed practice folders, cleaned up code

--- config/competition_seeds.js
+++ config/competition_seeds.js
@@ -1,149 +1,149 @@
 let competitionSeeds = [
-  {
-    name: "K9 Post",
-    email: "K9Post@gmail.com",
-    date: "15/02/2020",
-    location: "63105",
-    description: "Hardest hitting bite sport competition. Bring your best!",
-    group: "Working"
+	{
+		name: "K9 Post",
+		email: "K9Post@gmail.com",
+		date: "15/02/2020",
+		location: "63105",
+		description: "Hardest hitting bite sport competition. Bring your best!",
+		group: "Working"
 
-  },
-  {
-    name: "Jacksonville All-breed Conformation Show",
-    email: "AussieLover234@gmail.com",
-    date: "21/03/2020",
-    location: "32034",
-    description: "No Description",
-    group: "All Groups"
-  },
-  {
-    name: "IGP Trial",
-    email: "HarterHund@yahoo.com",
-    date: "18/04/2020",
-    location: "33186",
-    description: "Greater Miami Schutzhund club IGP trial. Judge Arthur Collins",
-    group: "Working"
-  },
-  {
-    name: "Schipperke Club of America National Specialty",
-    email: "sca@aol.com",
-    date: "19/04/2020",
-    location: "45177",
-    description: "Schipperke 2020 National Specialty hosted in conjunction with SMSC. Event will be held at host hotel.",
-    group: "Non-Sporting"
-  },
-  {
-    name: "Mastiff National Speciallty",
-    email: "MCAONS1973@",
-    date: "09/05/2020",
-    location: "14724",
-    description: "Come join us for the 73rd Mastif national Specailty! Hosted by Empire State Mastiff fanciers.",
-    group: "Working"
-  },
-  {
-    name: "Texas Quad 2020",
-    email: "bcollie21@aol.com",
-    date: "07/03/2020",
-    location: "77521",
-    description: "Skyhoundz Classic will occur on March 7th 2020 from 7am-7pm. We cannot wait to see all of our out-of-towners on this awesome day!",
-    group: "All Groups"
-  },
-  {
-    name: "Tamps Bay PSA Trial 2020",
-    email: "TBPSA@gmail.com",
-    date: "11/03/2020",
-    location: "33777",
-    description: "Tampa Bay is happy to kick off the PSA season with a killer trial. <br>Judge Janney Edwrds.<br> Decoys: Darryl Richey, Shawn Edwards, Jeff Riccio, Matt Taylor, Shaun Thankachen, and Kyle Murphy.",
-    group: "All Groups"
-  },
-  {
-    name: "GSDCA National Specialty",
-    email: "GSDCA@gmail.com",
-    date: "11/04/2020",
-    location: "63101",
-    description: "Please join us for the 73rd German Shepherd Dog National Specialy",
-    group: "Herding"
-  },
-  {
-    name: "SheepCamp Herding Trial",
-    email: "SheepCamp@gmail.com",
-    date: "28/04/2020",
-    location: "14201",
-    description: "SheepCamp presents their 3rd herding trial. Herding courses A, B, and C will be available. Entries close 04/01/2020.",
-    group: "Herding"
-  },
-{
-  name: "Belgium Sheepdog Club of America",
-  email: "letitbiteK9@gmail.com",
-  date: "28/03/2020",
-  location: "93536",
-  description: "FRENCH STYLE COURSE HERDING TRIAL Trials open to all AKC Registered and ILP/PAL Dogs of breeds approved by the AKC for Herding other than Border Collies & Kelpies.  Dogs must be 9 months or older. ",
-  group: "All Groups"
-},
-{
-  name: "Basset Hound of America Field Trial",
-  email: "Bassetit@gmail.com",
-  date: "07/06/2020",
-  location: "93536",
-  description: "In a Basset Hound Field Trial, dogs run in packs of two or more after a rabbit or hare. Don’t worry: The rabbits and hares are never touched. The whole purpose of the event is to exhibit your dog’s scenting skills, not his actual hunting skills. ",
-  group: "Hound"
-},
-{
-  name: "West Coast All-Breed Conformation Regional Show",
-  email: "WCRegionalChair@verizon.net",
-  date: "07/06/2020",
-  location: "43215",
-  description: "Come join your west coast friends for a day of fun at the West Coast Regional Classic!",
-  group: "All Groups"
-},
-{
-  name: "FAST CAT All Breed Trial",
-  email: "FASTCATTrialChair@verizon.net",
-  date: "03/03/2020",
-  location: "26886",
-  description: "Day of entries are accepted. Total of four runs!",
-  group: "All Groups"
-},
-{
-  name: "East coast Working Dog Trial",
-  email: "WorkingDogz@gmail.com",
-  date: "08/03/2020",
-  location: "90069",
-  description: "Judge George Townson will be Judging the all day event.",
-  group: "All Groups"
-},
-{
-  name: "All breed Agility",
-  email: "AgilityDoggoz@gmail.com",
-  date: "03/08/2020",
-  location: "61931",
-  description: "No description ",
-  group: "All Groups"
-},
-{
-  name: "Barn Hunt Trial",
-  email: "RatzSWF@gmail.com",
-  date: "19/08/2020",
-  location: "46107",
-  description: "No description ",
-  group: "All Groups"
-},
-{
-  name: "Strawberry Cluster Classic Conformation",
-  email: "StrawberyClubAB@gmail.com",
-  date: "15/07/2020",
-  location: "46107",
-  description: "No description ",
-  group: "All Groups"
-},
-{
-  name: "Canine Good Citizen Testing",
-  email: "StarzK9z@gmail.com",
-  date: "10/07/2020",
-  location: "34744",
-  description: "No description ",
-  group: "All Groups"
-}
+	},
+	{
+		name: "Jacksonville All-breed Conformation Show",
+		email: "AussieLover234@gmail.com",
+		date: "21/03/2020",
+		location: "32034",
+		description: "No Description",
+		group: "All Groups"
+	},
+	{
+		name: "IGP Trial",
+		email: "HarterHund@yahoo.com",
+		date: "18/04/2020",
+		location: "33186",
+		description: "Greater Miami Schutzhund club IGP trial. Judge Arthur Collins",
+		group: "Working"
+	},
+	{
+		name: "Schipperke Club of America National Specialty",
+		email: "sca@aol.com",
+		date: "19/04/2020",
+		location: "45177",
+		description: "Schipperke 2020 National Specialty hosted in conjunction with SMSC. Event will be held at host hotel.",
+		group: "Non-Sporting"
+	},
+	{
+		name: "Mastiff National Speciallty",
+		email: "MCAONS1973@",
+		date: "09/05/2020",
+		location: "14724",
+		description: "Come join us for the 73rd Mastif national Specailty! Hosted by Empire State Mastiff fanciers.",
+		group: "Working"
+	},
+	{
+		name: "Texas Quad 2020",
+		email: "bcollie21@aol.com",
+		date: "07/03/2020",
+		location: "77521",
+		description: "Skyhoundz Classic will occur on March 7th 2020 from 7am-7pm. We cannot wait to see all of our out-of-towners on this awesome day!",
+		group: "All Groups"
+	},
+	{
+		name: "Tamps Bay PSA Trial 2020",
+		email: "TBPSA@gmail.com",
+		date: "11/03/2020",
+		location: "33777",
+		description: "Tampa Bay is happy to kick off the PSA season with a killer trial. <br>Judge Janney Edwrds.<br> Decoys: Darryl Richey, Shawn Edwards, Jeff Riccio, Matt Taylor, Shaun Thankachen, and Kyle Murphy.",
+		group: "All Groups"
+	},
+	{
+		name: "GSDCA National Specialty",
+		email: "GSDCA@gmail.com",
+		date: "11/04/2020",
+		location: "63101",
+		description: "Please join us for the 73rd German Shepherd Dog National Specialy",
+		group: "Herding"
+	},
+	{
+		name: "SheepCamp Herding Trial",
+		email: "SheepCamp@gmail.com",
+		date: "28/04/2020",
+		location: "14201",
+		description: "SheepCamp presents their 3rd herding trial. Herding courses A, B, and C will be available. Entries close 04/01/2020.",
+		group: "Herding"
+	},
+	{
+		name: "Belgium Sheepdog Club of America",
+		email: "letitbiteK9@gmail.com",
+		date: "28/03/2020",
+		location: "93536",
+		description: "FRENCH STYLE COURSE HERDING TRIAL Trials open to all AKC Registered and ILP/PAL Dogs of breeds approved by the AKC for Herding other than Border Collies & Kelpies.  Dogs must be 9 months or older. ",
+		group: "All Groups"
+	},
+	{
+		name: "Basset Hound of America Field Trial",
+		email: "Bassetit@gmail.com",
+		date: "07/06/2020",
+		location: "93536",
+		description: "In a Basset Hound Field Trial, dogs run in packs of two or more after a rabbit or hare. Don’t worry: The rabbits and hares are never touched. The whole purpose of the event is to exhibit your dog’s scenting skills, not his actual hunting skills. ",
+		group: "Hound"
+	},
+	{
+		name: "West Coast All-Breed Conformation Regional Show",
+		email: "WCRegionalChair@verizon.net",
+		date: "07/06/2020",
+		location: "43215",
+		description: "Come join your west coast friends for a day of fun at the West Coast Regional Classic!",
+		group: "All Groups"
+	},
+	{
+		name: "FAST CAT All Breed Trial",
+		email: "FASTCATTrialChair@verizon.net",
+		date: "03/03/2020",
+		location: "26886",
+		description: "Day of entries are accepted. Total of four runs!",
+		group: "All Groups"
+	},
+	{
+		name: "East coast Working Dog Trial",
+		email: "WorkingDogz@gmail.com",
+		date: "08/03/2020",
+		location: "90069",
+		description: "Judge George Townson will be Judging the all day event.",
+		group: "All Groups"
+	},
+	{
+		name: "All breed Agility",
+		email: "AgilityDoggoz@gmail.com",
+		date: "03/08/2020",
+		location: "61931",
+		description: "No description ",
+		group: "All Groups"
+	},
+	{
+		name: "Barn Hunt Trial",
+		email: "RatzSWF@gmail.com",
+		date: "19/08/2020",
+		location: "46107",
+		description: "No description ",
+		group: "All Groups"
+	},
+	{
+		name: "Strawberry Cluster Classic Conformation",
+		email: "StrawberyClubAB@gmail.com",
+		date: "15/07/2020",
+		location: "46107",
+		description: "No description ",
+		group: "All Groups"
+	},
+	{
+		name: "Canine Good Citizen Testing",
+		email: "StarzK9z@gmail.com",
+		date: "10/07/2020",
+		location: "34744",
+		description: "No description ",
+		group: "All Groups"
+	}
 ];
 
 
